3935e2bd9004191c9e05a9e6c9e9e7dfcbdb8ef6
toewpq <helper@js.gripe>
2026-08-26 02:04:59 +0800

    fix: use blog.openjsu.com as canonical site

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,7 @@ import { defineConfig } from "astro/config";
 
 export default defineConfig({
   output: "static",
-  site: "https://blog.js.gripe",
+  site: "https://blog.openjsu.com",
   outDir: "./public",
   publicDir: "./static"
 });
